2fc4e4dd4fbe2d78f77ef80ff2e6c4af9362eb96
PmasonFF <petermijo@bmts.com>
2020-05-31 16:10:52 -0400

    Update link_from_list.py

--- Panoptes_client_examples/link_from_list.py
+++ Panoptes_client_examples/link_from_list.py
@@ -5,7 +5,7 @@ from panoptes_client import SubjectSet, Subject, Project, Panoptes
 
 #  modify path and file name as needed:
 manifest_file = r'C:\py\list_of_subject_ids_to_add.csv'
-# must have a column headed "subject_id" which is the list of subject to add
+# must have a column headed "subject_id" which is the list of subject to add/remove
 
 # set up zooniverse User_name and password as environmental variables
 Panoptes.connect(username=os.environ['User_name'], password=os.environ['Password'])
@@ -21,10 +21,9 @@ except StopIteration:
     print('Subject set', set_id, 'not found')
     quit()
 
-# This section adds subjects from a manifest to the above subject set
+# This section adds/removes subjects from a manifest to the above subject set
 with open(manifest_file, 'r') as mani_file:
     r = csv.DictReader(mani_file)
     for line in r:
-        #  modify the next three lines with the appropriate column headers from the manifest file
         subject_set.add(line['subject_id'])  #  note subjects can also be unlinked by changing "add" to "remove" in this line
         print(line['subject_id'])
